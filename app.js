c789f2ee139415264917d8b6c9766a767e8ab36f
Ericspk33 <ericspk33@gmail.com>
2021-03-16 16:42:32 -0700

    dates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { Discord, MessageEmbed } = require('discord.js');
 require('dotenv').config();
 
 const client = new Discord.Client();  
@@ -6,6 +6,8 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
     console.log(`I am online, my name is ${client.user.username}`);
+
+    // date stuff
     var date = new Date();
     var now = new Date(date.setTime(date.getTime() - (7 * 60 * 60 * 1000)));
     console.log(now.toLocaleString('en-US'));
@@ -14,15 +16,33 @@ client.on('ready', () => {
     console.log(now.getMinutes())
 
     if(now.getDay() === 2){ // tuesday
-        // if(){
-
-        // }
-        console.log("hihihi")
+        if(now.getHours() === 15){ // 3pm
+            if(now.getMinutes() === 0){ // 0mins
+                console.log("tue 3:00pm")
+            }
+        }
     }
 
+    if(now.getDay() === 3){ // wednesday
+        if(now.getHours() === 12){ // 12pm
+            if(now.getMinutes() === 30){ // 30mins
+                console.log("wed 12:30pm")
+            }
+        }
+    }
+
+    // specific channel
     if(1 == 2){
         client.channels.cache.get("821511906774876170").send(`I am online, my name is ${client.user.username}`);
     }
+
+    // message embed
+    const embed = new MessageEmbed()
+    .setTitle('Test Title')
+    .setColor(0xff000)
+    .setDescription('Test description');
+
+    client.channels.cache.get("821511906774876170").send(embed);
 });
 
 
