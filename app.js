80a8d478f3e36730909ccea561d452b0a6dbb856
Ericspk33 <ericspk33@gmail.com>
2021-03-16 17:01:10 -0700

    dates

--- app.js
+++ app.js
@@ -38,10 +38,58 @@ client.on('ready', () => {
     }
 
     // message embed
-    const embed = new Discord.MessageEmbed()
-    .setTitle('Test Title')
-    .setColor(0xff000)
-    .setDescription('Test description');
+    // const embed = new Discord.MessageEmbed()
+    // .setTitle('Test Title')
+    // .setColor(0x69f0ae)
+    // .setAuthor()
+    // .setDescription('Test description');
+
+    const embed = {
+        "title": "title ~~(did you know you can have markdown here too?)~~",
+        "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+        "url": "https://discordapp.com",
+        "color": 10385318,
+        "timestamp": "2021-03-16T23:57:58.379Z",
+        "footer": {
+          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+          "text": "footer text"
+        },
+        "thumbnail": {
+          "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        "image": {
+          "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        "author": {
+          "name": "author name",
+          "url": "https://discordapp.com",
+          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        "fields": [
+          {
+            "name": "🤔",
+            "value": "some of these properties have certain limits..."
+          },
+          {
+            "name": "😱",
+            "value": "try exceeding some of them!"
+          },
+          {
+            "name": "🙄",
+            "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+          },
+          {
+            "name": "<:thonkang:219069250692841473>",
+            "value": "these last two",
+            "inline": true
+          },
+          {
+            "name": "<:thonkang:219069250692841473>",
+            "value": "are inline fields",
+            "inline": true
+          }
+        ]
+      };
 
     client.channels.cache.get("821511906774876170").send(embed);
 });
